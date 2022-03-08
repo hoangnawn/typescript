82f2aa2c4285d5fff75bb001b1057af4ac71c9da
Hoang Nam <namhoangde2002@gmail.com>
2022-03-08 17:33:20 +0700

    feat: new

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const ten = "Hoang nam";
+const age = 20;
+const gioitinh = true;
+const obj = {};
+const numberArr = [1, 2, 3];
+const stringArr = ["a", "b"];
+const objectArr = [{ id: 1 }, { id: 2 }];
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-console.log("Hello World");
-const a: number = 10;
-const b: number = 30;
+
 
 const ten: string = "Hoang nam";
+const age: number = 20;
+const gioitinh : boolean = true;
+const obj: object = {};
+
+const numberArr : number[] = [1,2,3];
+const stringArr : string[] = ["a","b"];
+const objectArr: Object[] = [{id: 1},{id:2}]
