440bf5dce2dcbfa05d827296b441d4eee9974035
Hoang Nam <namhoangde2002@gmail.com>
2022-03-15 17:42:50 +0700

    creat

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const ten = "Hoang nam";
-const age = 20;
-const gioitinh = true;
-const obj = {};
-const numberArr = [1, 2, 3];
-const stringArr = ["a", "b"];
-const objectArr = [{ id: 1, name: "A" }, { id: 2, name: "B" }];
-//# sourceMappingURL=app.js.map
